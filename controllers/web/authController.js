493a0d82ac05b4afb3ed890efe3c591fbd0e6a12
kspsundar <kspsundar@gmail.com>
2026-09-19 15:36:14 +0530

    email notify update

--- controllers/web/authController.js
+++ controllers/web/authController.js
@@ -1,6 +1,6 @@
 const authService = require('../../services/authService');
 const { Address } = require('../../models');
-//const emailtest = require('../../haraka');
+const nodemailer = require("nodemailer");
 
 function sessionUser(user) {
   return { id: user.id, name: user.name, email: user.email, role: user.role };
@@ -27,11 +27,9 @@ exports.register = async (req, res) => {
     req.session.user = sessionUser(user);
     req.flash('success', 'Welcome! Your account has been created.');
     res.redirect('/');
-    console.log("EMAIL TEST !@#$!@#$!#@$@!$");
-    //emailtest();
-
-    const nodemailer = require("nodemailer")
-    async function sendMail() {
+    
+    
+      try {
       let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -39,24 +37,19 @@ exports.register = async (req, res) => {
           pass: 'uezk bmpb jajs cxrt'
         }
       });
-      let mail=""
       let mailOptions = {
         from: '"Adminstrator 09809808080" ui.interface@gmail.com', 
-        to: 'kannadasanswaminathan@gmail.com', 
+        to: user.email, 
         subject: 'User Registered',
         text: 'Hi',
-        html: '<p>Thank you for signing up with ABC store. Click the button below to verify your email and start shopping.</p>'
+        html: `<p>Thank you for signing up with ${user.name}.</p>`
       };
-    
-      try {
         let info = await transporter.sendMail(mailOptions);
         console.log('Message sent: %s', info.messageId);
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
       } catch (error) {
         console.error('Error sending email:', error);
-      }
-    }
-    sendMail();
+      }  
   } catch (err) {
     req.flash('error', err.message);
     res.redirect('/register');
